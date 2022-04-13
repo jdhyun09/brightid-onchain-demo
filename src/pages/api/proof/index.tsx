4b9914441711bc52e5863cc8fbce8b0dd63733e7
jdhyun09 <jdhyun09@naver.com>
2022-04-14 01:38:44 +0900

    1q122

--- src/pages/api/proof/index.tsx
+++ src/pages/api/proof/index.tsx
@@ -20,8 +20,8 @@ const handleMembershipProof = async (
 
   try {
     const zkFiles = {
-      wasmFilePath: path.join(dirname__+"/static/semaphore.wasm"),
-      zkeyFilePath: path.join(dirname__+"/static/semaphore_final.zkey")
+      wasmFilePath: path.join(__dirname+"/static/semaphore.wasm"),
+      zkeyFilePath: path.join(__dirname+"/static/semaphore_final.zkey")
     }
 
     const identity = new ZkIdentity(Strategy.MESSAGE, message)
